15fa24f149b1b3f88eea7407954fc1fa606a87f6
riporipoteam-ctrl <ripo.ripoteam@gmail.com>
2026-07-31 21:44:38 +0200

    Add browser media compatibility typing

--- src/types/webrtc-compat.d.ts
+++ src/types/webrtc-compat.d.ts
@@ -2,3 +2,8 @@ interface RTCOfferOptions {
   offerToReceiveAudio?: boolean;
   offerToReceiveVideo?: boolean;
 }
+
+/** Safari exposes playsInline on media elements at runtime, while some DOM typings omit it for audio. */
+interface HTMLAudioElement {
+  playsInline: boolean;
+}
